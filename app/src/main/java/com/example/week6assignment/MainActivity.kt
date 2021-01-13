4c5b0d81307aa4d9f545e6343ee854dd6cb1d49d
Nirajan <nirajangautam123@gmail.com>
2021-01-13 15:55:50 +0545

    Fragment completed

--- app/src/main/java/com/example/week6assignment/MainActivity.kt
+++ app/src/main/java/com/example/week6assignment/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.week6assignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_add_student, R.id.navigation_about_us))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/week6assignment/model/Database.kt
@@ -0,0 +1,4 @@
+package com.example.week6assignment.model
+
+class Database {
+}
